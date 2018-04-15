9c67df5202343ebf3c6facf011960655a1dc4605
Ahmed Boujelben <ahmed.hamdi.boujelben@gmail.com>
2018-04-15 20:03:12 +0100

    added find, find_if

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ auto main() -> int
     const auto n = size_t{100000000};
     auto src = std::vector<int>(n);
     std::generate(src.begin(), src.end(), []() {
-    return int(std::rand());
+        auto min(0);
+        auto max(50000);
+        return  min + (rand() % static_cast<int>(max - min + 1));
     });
 
     //auto dst = std::vector<float>(n);
@@ -40,13 +42,15 @@ auto main() -> int
         const ClockType::time_point start_ = ClockType::now();
 
         //par_for_each(src.begin(), src.end(), transform_func, chunk_sz);
-        auto f = par_count_if(src.begin(), src.end(), [](int val){return val%7==0;}, chunk_sz);
+        auto foundIt = par_find_if(src.begin(), src.end(), [](int a)->bool{return int(sqrt(sqrt(sqrt(sqrt(a)))))==13;}, chunk_sz);
+        //auto foundInt = foundIt==src.end()?-99999:*foundIt;
 
         auto stop = ClockType::now();
         auto duration = (stop - start_);
         auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-        std::cout << f<<" "<< chunk_sz<<": "<< ms << " ms " << '\n';
+        std::cout << foundIt-src.begin() <<" chunk: "<<chunk_sz<<": "<< ms << " ms " << '\n';
     }
 
+    std::cout << find_if(src.begin(), src.end(), [](int a)->bool{return int(sqrt(sqrt(sqrt(sqrt(a)))))==13;})-src.begin() << '\n';
 
 }
--- parallel.h
+++ parallel.h
@@ -7,239 +7,293 @@ namespace ABParallel {
 
 template <typename srcIt, typename dstIt, typename functor>
 auto par_transform(srcIt first, srcIt last, dstIt dst, functor func, size_t chunkSize) -> void {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    std::transform(first, last, dst, func);
-    return;
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        std::transform(first, last, dst, func);
+        return;
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=, &func] {
-    par_transform(first, srcMiddle, dst, func, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=, &func] {
+        par_transform(first, srcMiddle, dst, func, chunkSize);
+    });
 
-  // Recursively handle the second part
-  const auto dstMiddle = std::next(dst, n / 2);
-  par_transform(srcMiddle, last, dstMiddle, func, chunkSize);
-  future.wait();
+    // Recursively handle the second part
+    const auto dstMiddle = std::next(dst, n / 2);
+    par_transform(srcMiddle, last, dstMiddle, func, chunkSize);
+    future.wait();
 }
 
 // Parallel version of std::for_each
 
 template <typename srcIt, typename functor>
 auto par_for_each(srcIt first, srcIt last, functor func, size_t chunkSize) -> void {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    std::for_each(first, last, func);
-    return;
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        std::for_each(first, last, func);
+        return;
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=, &func] {
-    par_for_each(first, srcMiddle, func, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=, &func] {
+        par_for_each(first, srcMiddle, func, chunkSize);
+    });
 
-  // Recursively handle the second part
-  par_for_each(srcMiddle, last, func, chunkSize);
-  future.wait();
+    // Recursively handle the second part
+    par_for_each(srcMiddle, last, func, chunkSize);
+    future.wait();
+}
+
+// Parallel version of std::find
+
+template <typename srcIt, typename valueType>
+auto par_find(srcIt first, srcIt last, const valueType& value, size_t chunkSize) -> srcIt {
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::find(first, last, value);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
+
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=] () -> srcIt {
+        return par_find(first, srcMiddle, value, chunkSize);
+    });
+
+    // Recursively handle the second part
+    auto itFoundBySecondPart=par_find(srcMiddle, last, value, chunkSize);
+    auto itFoundByFirstPart=future.get();
+
+    // Return the first element found in the container
+    if(itFoundByFirstPart!=srcMiddle)
+        return itFoundByFirstPart;
+    if(itFoundByFirstPart==srcMiddle && itFoundBySecondPart!=last)
+        return itFoundBySecondPart;
+    return last;
+}
+
+// Parallel version of std::find_if
+
+template <typename srcIt, typename functor>
+auto par_find_if(srcIt first, srcIt last, functor func, size_t chunkSize) -> srcIt {
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::find_if(first, last, func);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
+
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=, &func] () -> srcIt {
+        return par_find_if(first, srcMiddle, func, chunkSize);
+    });
+
+    // Recursively handle the second part
+    auto itFoundBySecondPart=par_find_if(srcMiddle, last, func, chunkSize);
+    auto itFoundByFirstPart=future.get();
+
+    // Return the first element found in the container
+    if(itFoundByFirstPart!=srcMiddle)
+        return itFoundByFirstPart;
+    if(itFoundByFirstPart==srcMiddle && itFoundBySecondPart!=last)
+        return itFoundBySecondPart;
+    return last;
 }
 
 // Parallel version of std::fill
 
 template <typename srcIt, typename valueType>
 auto par_fill(srcIt first, srcIt last, const valueType& value, size_t chunkSize) -> void {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    std::fill(first, last, value);
-    return;
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        std::fill(first, last, value);
+        return;
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=] {
-    par_fill(first, srcMiddle, value, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=] {
+        par_fill(first, srcMiddle, value, chunkSize);
+    });
 
-  // Recursively handle the second part
-  par_fill(srcMiddle, last, value, chunkSize);
-  future.wait();
+    // Recursively handle the second part
+    par_fill(srcMiddle, last, value, chunkSize);
+    future.wait();
 }
 
 // Parallel version of std::copy
 
 template <typename srcIt, typename dstIt>
 auto par_copy(srcIt first, srcIt last, dstIt dst, size_t chunkSize) -> void {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    std::copy(first, last, dst);
-    return;
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        std::copy(first, last, dst);
+        return;
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=] {
-    par_copy(first, srcMiddle, dst, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=] {
+        par_copy(first, srcMiddle, dst, chunkSize);
+    });
 
-  // Recursively handle the second part
-  const auto dstMiddle = std::next(dst, n / 2);
-  par_copy(srcMiddle, last, dstMiddle, chunkSize);
-  future.wait();
+    // Recursively handle the second part
+    const auto dstMiddle = std::next(dst, n / 2);
+    par_copy(srcMiddle, last, dstMiddle, chunkSize);
+    future.wait();
 }
 
 // Parallel version of std::copy_if
 
 template <typename srcIt, typename dstIt, typename functor>
 auto par_copy_if(srcIt first, srcIt last, dstIt dst, functor func, size_t chunkSize) -> dstIt {
-  auto n = static_cast<size_t>(std::distance(first, last));
-  using copiedRange = std::pair<dstIt, dstIt>;
-  using futureType = std::future< copiedRange >;
-  auto futures = std::vector<futureType>{};
-  futures.reserve(n / chunkSize);
+    auto n = static_cast<size_t>(std::distance(first, last));
+    using copiedRange = std::pair<dstIt, dstIt>;
+    using futureType = std::future< copiedRange >;
+    auto futures = std::vector<futureType>{};
+    futures.reserve(n / chunkSize);
 
-  for (size_t startId = 0; startId < n; startId += chunkSize) {
-    const auto stopId = std::min(startId + chunkSize, n);
-    auto future = std::async(std::launch::async, [=, &func] {
-      auto dstFirst = dst + startId;
-      auto dstLast = std::copy_if(first + startId, first + stopId,
-      dstFirst, func);
-      return std::make_pair(dstFirst, dstLast);
-    });
-    futures.emplace_back(std::move(future));
-  }
+    for (size_t startId = 0; startId < n; startId += chunkSize) {
+        const auto stopId = std::min(startId + chunkSize, n);
+        auto future = std::async(std::launch::async, [=, &func] {
+            auto dstFirst = dst + startId;
+            auto dstLast = std::copy_if(first + startId, first + stopId,
+                                        dstFirst, func);
+            return std::make_pair(dstFirst, dstLast);
+        });
+        futures.emplace_back(std::move(future));
+    }
 
-  auto dstLast = futures.front().get().second;
-  for (auto it = std::next(futures.begin()); it != futures.end(); ++it) {
-    auto chunkRange = it->get();
-    dstLast = std::move(chunkRange.first, chunkRange.second, dstLast);
-  }
-  return dstLast;
+    auto dstLast = futures.front().get().second;
+    for (auto it = std::next(futures.begin()); it != futures.end(); ++it) {
+        auto chunkRange = it->get();
+        dstLast = std::move(chunkRange.first, chunkRange.second, dstLast);
+    }
+    return dstLast;
 }
 
 // Parallel version of std::count
 
 template <typename srcIt, typename valueType>
 auto par_count(srcIt first, srcIt last, const valueType& value, size_t chunkSize) -> typename std::iterator_traits<srcIt>::difference_type {
-  using counterType=typename std::iterator_traits<srcIt>::difference_type;
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    return std::count(first, last, value);
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    using counterType=typename std::iterator_traits<srcIt>::difference_type;
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::count(first, last, value);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=] () -> counterType {
-      return par_count(first, srcMiddle, value, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=] () -> counterType {
+        return par_count(first, srcMiddle, value, chunkSize);
+    });
 
-  // Recursively handle the second part
-  auto count1=par_count(srcMiddle, last, value, chunkSize);
-  auto count2=future.get();
-  return count1+count2;
+    // Recursively handle the second part
+    auto count1=par_count(srcMiddle, last, value, chunkSize);
+    auto count2=future.get();
+    return count1+count2;
 }
 
 // Parallel version of std::count_if
 
 template <typename srcIt, typename functor>
 auto par_count_if(srcIt first, srcIt last, functor func, size_t chunkSize) -> typename std::iterator_traits<srcIt>::difference_type {
-  using counterType=typename std::iterator_traits<srcIt>::difference_type;
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    return std::count_if(first, last, func);
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    using counterType=typename std::iterator_traits<srcIt>::difference_type;
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::count_if(first, last, func);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=, &func] () -> counterType {
-      return par_count_if(first, srcMiddle, func, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=, &func] () -> counterType {
+        return par_count_if(first, srcMiddle, func, chunkSize);
+    });
 
-  // Recursively handle the second part
-  auto count1=par_count_if(srcMiddle, last, func, chunkSize);
-  auto count2=future.get();
-  return count1+count2;
+    // Recursively handle the second part
+    auto count1=par_count_if(srcMiddle, last, func, chunkSize);
+    auto count2=future.get();
+    return count1+count2;
 }
 
 // Parallel version of std::max_element
 
 template <typename srcIt>
 auto par_max_element(srcIt first, srcIt last, size_t chunkSize) -> srcIt {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    return std::max_element(first, last);
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::max_element(first, last);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=] () -> srcIt {
-      return par_max_element(first, srcMiddle, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=] () -> srcIt {
+        return par_max_element(first, srcMiddle, chunkSize);
+    });
 
-  // Recursively handle the second part
-  auto max1=par_max_element(srcMiddle, last, chunkSize);
-  auto max2=future.get();
-  return (*max2<*max1)?max1:max2;
+    // Recursively handle the second part
+    auto max1=par_max_element(srcMiddle, last, chunkSize);
+    auto max2=future.get();
+    return (*max2<*max1)?max1:max2;
 }
 
 template <typename srcIt, typename functor>
 auto par_max_element(srcIt first, srcIt last, functor func, size_t chunkSize) -> srcIt {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    return std::max_element(first, last, func);
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::max_element(first, last, func);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=, &func] () -> srcIt {
-    return par_max_element(first, srcMiddle, func, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=, &func] () -> srcIt {
+        return par_max_element(first, srcMiddle, func, chunkSize);
+    });
 
-  // Recursively handle the second part
-  auto max1=par_max_element(srcMiddle, last, func, chunkSize);
-  auto max2=future.get();
-  return func(*max2,*max1)?max1:max2;
+    // Recursively handle the second part
+    auto max1=par_max_element(srcMiddle, last, func, chunkSize);
+    auto max2=future.get();
+    return func(*max2,*max1)?max1:max2;
 }
 
 // Parallel version of std::min_element
 
 template <typename srcIt>
 auto par_min_element(srcIt first, srcIt last, size_t chunkSize) -> srcIt {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    return std::min_element(first, last);
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::min_element(first, last);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=] () -> srcIt {
-      return par_min_element(first, srcMiddle, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=] () -> srcIt {
+        return par_min_element(first, srcMiddle, chunkSize);
+    });
 
-  // Recursively handle the second part
-  auto min1=par_min_element(srcMiddle, last, chunkSize);
-  auto min2=future.get();
-  return (*min2<=*min1)?min2:min1;
+    // Recursively handle the second part
+    auto min1=par_min_element(srcMiddle, last, chunkSize);
+    auto min2=future.get();
+    return (*min2<=*min1)?min2:min1;
 }
 
 template <typename srcIt, typename functor>
 auto par_min_element(srcIt first, srcIt last, functor func, size_t chunkSize) -> srcIt {
-  const auto n = static_cast<size_t>(std::distance(first, last));
-  if (n <= chunkSize) {
-    return std::min_element(first, last, func);
-  }
-  const auto srcMiddle = std::next(first, n / 2);
+    const auto n = static_cast<size_t>(std::distance(first, last));
+    if (n <= chunkSize) {
+        return std::min_element(first, last, func);
+    }
+    const auto srcMiddle = std::next(first, n / 2);
 
-  // Branch of first part to another task
-  auto future = std::async(std::launch::async, [=, &func] () -> srcIt {
-    return par_min_element(first, srcMiddle, func, chunkSize);
-  });
+    // Branch of first part to another task
+    auto future = std::async(std::launch::async, [=, &func] () -> srcIt {
+        return par_min_element(first, srcMiddle, func, chunkSize);
+    });
 
-  // Recursively handle the second part
-  auto min1=par_min_element(srcMiddle, last, func, chunkSize);
-  auto min2=future.get();
-  return func(*min2,*min1)?min2:min1;
+    // Recursively handle the second part
+    auto min1=par_min_element(srcMiddle, last, func, chunkSize);
+    auto min2=future.get();
+    return func(*min2,*min1)?min2:min1;
 }
 
 // Parallel algorithm that calculates the sum of elements inside a container
@@ -249,13 +303,13 @@ auto par_sum(srcIt first, srcIt last, size_t chunkSize) -> typename std::iterato
     using valueType=typename std::iterator_traits<srcIt>::value_type;
     const auto n = static_cast<size_t>(std::distance(first, last));
     if (n <= chunkSize) {
-    return std::accumulate(first, last, static_cast<valueType>(0));
+        return std::accumulate(first, last, static_cast<valueType>(0));
     }
     const auto srcMiddle = std::next(first, n / 2);
 
     // Branch of first part to another task
     auto future = std::async(std::launch::async, [=] () -> valueType {
-      return par_sum(first, srcMiddle, chunkSize);
+        return par_sum(first, srcMiddle, chunkSize);
     });
 
     // Recursively handle the second part
@@ -280,7 +334,7 @@ auto par_sum(srcIt first, srcIt last, functor func, size_t chunkSize) -> typenam
 
     // Branch of first part to another task
     auto future = std::async(std::launch::async, [=,&func] () -> valueType {
-      return par_sum(first, srcMiddle, func, chunkSize);
+        return par_sum(first, srcMiddle, func, chunkSize);
     });
 
     // Recursively handle the second part
